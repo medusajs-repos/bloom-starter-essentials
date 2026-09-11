4f4980988bb2e968a0498c5cfca92768c63f3b67
Shahed Nasser <shahednasser@gmail.com>
2026-09-11 13:53:00 +0300

    fixes

--- apps/storefront/src/components/search/predictive-search.tsx
+++ apps/storefront/src/components/search/predictive-search.tsx
@@ -18,7 +18,7 @@ export const PredictiveSearch = () => {
     query_params: {
       q: query,
       limit: 6,
-      fields: "id,title,handle,thumbnail,calculated_price",
+      fields: "id,title,handle,thumbnail,*variants.calculated_price",
     },
     region_id: region?.id,
   })
